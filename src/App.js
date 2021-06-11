b7ca1d64879f8a66613eaa5cd9c09d79ab961354
josh sorensen <jxsoren@gmail.com>
2021-06-10 21:16:33 -0600

    html header changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Background from './components/Background/BG'
 import Home from './components/Home/Home'
 import './styles/Index.css'
 
+
 export default function App(){
+
     return(
         <>
             <Background />
